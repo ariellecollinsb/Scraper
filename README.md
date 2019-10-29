948be7477957a3c050256e6fa3f728fd5220605f
Arielle <ariellcollinsb@gmail.com>
2019-10-29 00:02:07 -0600

    working on app logic

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Scraper
+
+## Author
+
+Arielle Collins
+
+Email: ariellecollinsb@gmail.com
+
+Github: https://github.com/ariellecollinsb
+
+LinkedIn: https://www.linkedin.com/in/arielle-collins-bovatsek-10895899/
+
+## Overview
+
+In this assignment, you'll create a web app that lets users view and leave comments on the latest news. But you're not going to actually write any articles; instead, you'll flex your Mongoose and Cheerio muscles to scrape news from another site.
+
+## Technologies Used
+
+Node.js,
+Morgan,
+Express + Express Handlebars
+Mongoose
+Cheerio
+Axios
+Other NPM command line utility + packages
+
+## Functionality
+
+App functionality depends on user input and table data.
+The table is printed to the screen.
+The user has the option to input a new burger name, or devour an existing one.
+A delete button allows the user to delete any burger they don't like from the list.
+
+## Future Development
+
+- Convert CSS to SCSS
--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("/scrape", function(req, res) {
     axios.get("http://www.huffpost.com/").then(function(response) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
-
+        console.log(response.data);
         // Now, we grab every h2 within an article tag, and do the following:
         $("div.card__content").each(function(i, element) {
             // Save an empty result object
@@ -56,37 +56,68 @@ app.get("/scrape", function(req, res) {
                 })
                 .catch(function(err) {
                     // If an error occurred, log it
-                    console.log(err);
+                    res.json(err);
                 });
         });
-
         // Send a message to the client
+        console.log()
         res.send("Scrape Complete");
     });
 });
 
+app.get("/", function(req, res) {
+    db.index.find({}, function(err, data) {
+        if (err) return res.status(500).end();
+    })
+    res.json(data);
+});
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-    // TODO: Finish the route so it grabs all of the articles
+app.get("/article", function(req, res) {
+    db.articles.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        })
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-    // TODO
-    // ====
-    // Finish the route so it finds one article using the req.params.id,
-    // and run the populate method with "note",
-    // then responds with the article with the note included
+app.get("/comment", function(req, res) {
+    db.Comment.find({})
+        .then(function(dbComment) {
+            res.json(dbComment);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
 });
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/article/:id", function(req, res) {
+    var id = req.params.id
+    db.articles.findById(id)
+        .populate("comment")
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+// Finish the route so it finds one article using the req.params.id,
+// and run the populate method with "note",
+// then responds with the article with the note included
+
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+app.post("/article/:id", function(req, res) {
     // TODO
     // ====
     // save the new note that gets posted to the Notes collection
     // then find an article from the req.params.id
     // and update it's "note" property with the _id of the new note
 });
+
 
 // Start the server
 app.listen(PORT, function() {
